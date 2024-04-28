aeb678dee30c89a0c26708b68a38fc5828d9f0b2
Shawn Sudaria <shawnlovecode14@gmail.com>
2024-04-28 17:37:38 +0800

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    """
+    Didi ig butang main stuff, mostly function calling lang. Aton functionality kay ig split 
+    up into components and ig butang ha modules function
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
